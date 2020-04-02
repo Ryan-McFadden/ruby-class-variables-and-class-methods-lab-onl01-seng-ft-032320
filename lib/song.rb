a1f8feb3a6bbe0da6c389d958fe576dc7cca1a79
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-02 20:20:27 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -6,6 +6,9 @@ class Song
   @@artists = []
   
   def initialize(name, artist, genre)
+    @name = name 
+    @artist = artist
+    @genre = genre
     @@count += 1 
     @@genres << genre unless @@genres.include?(genre)
     @@artists << artist unless @@artists.include?(artist)
