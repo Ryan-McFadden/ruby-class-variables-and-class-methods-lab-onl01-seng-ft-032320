e9004f0a4d6f67878aa726e578ec878028b225bc
Ryan McFadden <ryanmcfadden123@gmail.com>
2020-04-02 20:07:09 +0000

    Automatically backed up by Learn

---
+++ lib/song.rb
@@ -0,0 +1,36 @@
+class Song 
+  attr_accessor :name, :artist, :genre 
+  
+  @@count = 0 
+  @@genres = []
+  @@artists = []
+  
+  def initialize(name, artist, genre)
+    @@count += 1 
+    @@genres << genre unless @@genres.include?(genre)
+    @@artists << artist unless @@artists.include?(artist)
+  end
+  
+  def name
+    @name
+  end
+  
+  def self.count 
+    @@count
+  end
+  
+  def self.genres
+    @@genres 
+  end
+  
+  def self.artists
+    @@artists
+  end
+  
+  def self.genre_count
+    num_of_songs = {}
+    
+    
+  end
+
+end
